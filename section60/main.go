2d1c8257fc1365231073f0c7ca89c7549993aa97
junstory <lastcom1@gmail.com>
2024-11-12 01:37:04 +0900

    week5

--- section60/main.go
+++ section60/main.go
@@ -5,17 +5,26 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/junstory/go_todo_app/section60/config"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server %v", err)
+		os.Exit(1)
 	}
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -28,5 +37,25 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("server is running at %s", url)
 
-	return nil
+	s := &http.Server{
+		//Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.Serve(l); /*s.ListenAndServe()*/ err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
 }
