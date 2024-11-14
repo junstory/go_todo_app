5da1ad2d6acf02038e140472eefd7213c260bc66
junstory <lastcom1@gmail.com>
2024-11-14 22:56:22 +0900

    Create. gRPC test

---
+++ sideProject/grpc-demo/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "grpc-demo/proto"
+
+	"google.golang.org/grpc"
+)
+
+type greeterServer struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *greeterServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", req.GetName())
+	return &pb.HelloReply{Message: "Hello " + req.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &greeterServer{})
+
+	log.Println("Server is running on port 50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
