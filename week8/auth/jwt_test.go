3bc67800a71c0747f83558b8f20e821ae5ba1807
junstory <lastcom1@gmail.com>
2024-11-26 03:14:39 +0900

    Section84

--- week8/auth/jwt_test.go
+++ week8/auth/jwt_test.go
@@ -3,11 +3,21 @@ package auth
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/junstory/go_todo_app/week8/clock"
 	"github.com/junstory/go_todo_app/week8/entity"
+	"github.com/junstory/go_todo_app/week8/store"
 	"github.com/junstory/go_todo_app/week8/testutil/fixture"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 )
 
 func TestEmbed(t *testing.T) {
@@ -39,9 +49,141 @@ func TestJWTer_GenJWT(t *testing.T) {
 	}
 	got, err := sut.GenerateToken(ctx, *u)
 	if err != nil {
-		t.Fatal("not want err: %v", err)
+		t.Fatalf("not want err: %v", err)
 	}
 	if len(got) == 0 {
 		t.Errorf("want token, but got empty")
+	}
+}
+
+func TestJWTer_GetJWT(t *testing.T) {
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+	want, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/junstory/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(c.Now()).
+		Expiration(c.Now().Add(30*time.Minute)).
+		Claim(RoleKey, "test").
+		Claim(UserNameKey, "test_user").
+		Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pkey, err := jwk.ParseKey(rawPrivKey, jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+	signed, err := jwt.Sign(want, jwt.WithKey(jwa.RS256, pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	userID := entity.UserID(20)
+	ctx := context.Background()
+	moq := &StoreMock{}
+	moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+		return userID, nil
+	}
+	sut, err := NewJWTer(moq, c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		`https://github.com/junstory`,
+		nil,
+	)
+	req.Header.Set(`Authorization`, fmt.Sprintf(`Bearer %s`, signed))
+	got, err := sut.GetToken(ctx, req)
+	if err != nil {
+		t.Fatalf("want no error, but got %v", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("GetToken() got = %v, want %v", got, want)
+	}
+}
+
+type FixedTomorrowClocker struct{}
+
+func (c FixedTomorrowClocker) Now() time.Time {
+	return clock.FixedClocker{}.Now().Add(24 * time.Hour)
+}
+
+func TestJWTer_GetJWT_NG(t *testing.T) {
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+	tok, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/junstory/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(c.Now()).
+		Expiration(c.Now().Add(30*time.Minute)).
+		Claim(RoleKey, "test").
+		Claim(UserNameKey, "test_user").
+		Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pkey, err := jwk.ParseKey(rawPrivKey, jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+	signed, err := jwt.Sign(tok, jwt.WithKey(jwa.RS256, pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type moq struct {
+		userID entity.UserID
+		err    error
+	}
+	tests := map[string]struct {
+		c   clock.Clocker
+		moq moq
+	}{
+		"expire": {
+			//토큰의 만료 시간보다 미래 시간 반환.
+			c: FixedTomorrowClocker{},
+		},
+		"notFoundInStore": {
+			c: clock.FixedClocker{},
+			moq: moq{
+				err: store.ErrNotFound,
+			},
+		},
+	}
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			moq := &StoreMock{}
+			moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+				return tt.moq.userID, tt.moq.err
+			}
+			sut, err := NewJWTer(moq, tt.c)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req := httptest.NewRequest(
+				http.MethodGet,
+				`https://github.com/junstory`,
+				nil,
+			)
+			req.Header.Set(`Authorization`, fmt.Sprintf(`Bearer %s`, signed))
+			got, err := sut.GetToken(ctx, req)
+			if err == nil {
+				t.Errorf("want error, but got nil")
+			}
+			if got != nil {
+				t.Errorf("want nil, but got %v", got)
+			}
+		})
 	}
 }
