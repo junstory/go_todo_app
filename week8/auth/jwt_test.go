fe21952722c098e45ed39001e10c17522f50dccd
junstory <lastcom1@gmail.com>
2024-11-26 02:32:21 +0900

    Section84

--- week8/auth/jwt_test.go
+++ week8/auth/jwt_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestEmbed(t *testing.T) {
-	want := []byte("------BEGIN PUBLIC KEY-----")
+	want := []byte("-----BEGIN PUBLIC KEY-----")
 	if !bytes.Contains(rawPubKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPubKey)
 	}
-	want = []byte("------BEGIN PRIVATE KEY-----")
+	want = []byte("-----BEGIN PRIVATE KEY-----")
 	if !bytes.Contains(rawPrivKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPubKey)
 	}
